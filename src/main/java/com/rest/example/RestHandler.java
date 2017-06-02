bed7a01eac517fa28dfe0550912dd5f79258d94a
saiba09 <saiba_sheikh@persistent.co.in>
2017-06-02 09:52:52 +0530

    test 7

--- src/main/java/com/rest/example/RestHandler.java
+++ src/main/java/com/rest/example/RestHandler.java
@@ -13,7 +13,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 public class RestHandler {
-//	public static String getData(){
+	//	public static String getData(){
 	public static void main(String[] args){
 		String sql ="INSERT INTO `healthcare-12:Mihin_Data_Sample.PatientDataFromRest` ( patient_id, name, Address, city, State, maritialStatus, telecom, gender, birthDate, multipleBirthInteger, deceasedDateTime ) VALUES ";
 		String values="";
@@ -39,26 +39,37 @@ public class RestHandler {
 					Gson gson = new GsonBuilder().create();
 					System.out.println(patientEntry.get(i).toString());
 					Patient p = gson.fromJson(patientEntry.get(i).toString(), Patient.class);
-					System.out.println("id" + p.getResourceObject().getId());
-					System.out.println("add :" + p.getResourceObject().getAddress()[0].toString() );
-					System.out.println("mar :"+p.getResourceObject().getMaritialStatus());
-					System.out.println("tele :" + p.getResourceObject().getTelecom()[0].toString());
-					System.out.println("gen :"+ p.getResourceObject().getGender() + "  : dob "+ p.getResourceObject().getBirthDate());
-					values += "( '" +p.getResourceObject().getId()+"' , '"+p.getResourceObject().getName()[0].toString() +
-							"' , '"+p.getResourceObject().getAddress()[0].toString() + "' , '"+p.getResourceObject().getAddress()[0].getCity() +"' , '"+
-							p.getResourceObject().getAddress()[0].getState()+"' , '"+p.getResourceObject().getMaritialStatus().toString()+"' , '"+ p.getResourceObject().getTelecom()[0].toString()+"' , '" + 
-							p.getResourceObject().getGender()+ "' , '" +p.getResourceObject().getBirthDate()+"' , '"+p.getResourceObject().getMultipleBirthInteger()+"' , '" +
+					values +=  "( '" +p.getResourceObject().getId()+"' , '" ;
+					if(p.getResourceObject().getName() != null){
+						values += p.getResourceObject().getName()[0].toString() +"' , '";
+					}
+					else values += "null' , '" ; 
+					if( p.getResourceObject().getAddress() != null ){
+						values += p.getResourceObject().getAddress()[0].toString() + "' , '"+p.getResourceObject().getAddress()[0].getCity() +"' , '"+
+								p.getResourceObject().getAddress()[0].getState()+"' , '";
+					}
+					else values += "null' , 'null' , 'null' ," ; 
+					if(p.getResourceObject().getMaritialStatus() != null)
+						values += p.getResourceObject().getMaritialStatus().toString()+"' , '";
+					else values += "null' , '" ; 
+					if (p.getResourceObject().getTelecom() != null) {
+						values += p.getResourceObject().getTelecom()[0].toString()+"' , '";
+					}
+					else values += "null' , '" ; 
+					values += p.getResourceObject().getGender()+ "' , '" +p.getResourceObject().getBirthDate()+"' , '"+p.getResourceObject().getMultipleBirthInteger()+"' , '" +
 							p.getResourceObject().getDeceasedDateTime()+"' ) ,";
-				System.out.println("p string"+p.toString());
+
+
+					System.out.println("p string"+p.toString());
 					System.out.println("Values" +values);
 				}
-				
-			values = values.substring(0, values.lastIndexOf(')')+1) ;
-			System.out.println("sql" +sql);
-			System.out.println("values 123" + values);
-			//return sql+values;
+
+				values = values.substring(0, values.lastIndexOf(')')+1) ;
+				System.out.println("sql" +sql);
+				System.out.println("values 123" + values);
+				//return sql+values;
 			}
-			
+
 
 		}catch(Exception ex){
 			System.out.println("exception : " + ex.getMessage());
